354f606cadcfe16cc1218b817d2e36c9769988a2
Daniel Mott <daniel.r.mott@gmail.com>
2025-02-03 20:11:55 -0800

    win check and alert working

--- src/main.js
+++ src/main.js
@@ -49,6 +49,7 @@ const GameController = (function () {
 
   const passTurn = () => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    console.log(`${getActivePlayer().name}'s turn.`);
   };
 
   const getActivePlayer = () => activePlayer;
@@ -69,13 +70,44 @@ const GameController = (function () {
       } else {
         e.target.innerHTML = getActivePlayer().token;
         e.target.classList.add("fullCell");
+        e.target.classList.add(getActivePlayer().token);
+        if (winCheck()) {
+          alert(`${getActivePlayer().name} has won the game!`);
+        };
         passTurn();
-        console.log(`${getActivePlayer().name}'s turn.`);
+
+
       }
     }
   };
 
   boardContainer.addEventListener("click", pickCell);
+
+
+  const winCheck = () => {
+    const winPatterns = [
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 5, 9],
+      [3, 5, 7]
+    ];
+
+    for (const pattern of winPatterns) {
+      const [id1, id2, id3] = pattern;
+      const cell1 = document.getElementById(id1).innerHTML;
+      const cell2 = document.getElementById(id2).innerHTML;
+      const cell3 = document.getElementById(id3).innerHTML;
+      if (cell1 && cell1 === cell2 && cell2 === cell3) {
+return true;
+      }
+    }
+return false;
+  }
+
 
 })();
 
